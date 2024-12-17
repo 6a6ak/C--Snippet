10db94f39201f1d6ef57c97488465e877498e444
6a6ak <babak.ahari@gmail.com>
2024-12-18 00:30:51 +0100

    arr

---
+++ Basic/Hello_World/array01.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    std::cout << arr[3] << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+//using namespace std;
+
+int main(){
+    // cout << "Hello my name is Stefan" << endl;
+    // cout << "Testgatan 12" << endl;
+    // cout << "111 22  Teststad" << endl;
+
+    // 12 + 15
+    // 12 + 15 + 99
+    // cout    << "Hello my name is Stefan" << endl   
+    //         << "Testgatan 12" << endl 
+    //         << "111 22  Teststad" << endl;
+    //std::cout << "Hello my name is viktor" << std::endl << "Testgatan 12" << std::endl << "hej jag heter viktor" << std::endl;
+    //std::cout << "testgatan 12" << std::endl; 
+
+    // int prisPerNatt;
+    // int nätter;
+    // //int totalPrice = prisPerNatt * nätter;
+    // std::cout << "Vad kostar hotellet per natt?" << std::endl;
+    // std::cin >> prisPerNatt; 
+    // std::cout << "Hur många nätter har dut änkt att sova" << std::endl;
+    // std::cin >> nätter;
+    // int totalPrice = prisPerNatt * nätter;
+    // std::cout << "Det totala priset blir:" << totalPrice << std::endl;
+    // std::string forName;
+    // std::string lastName;
+    // std::cout << "Whats your first name" << std::endl;
+    // std::cin >> forName;
+    // std::cout << "Whats your last name" << std::endl;
+    // std::cin >> lastName;
+    // std::cout << "Your last name is " << lastName << ", your first name is " << forName;
+
+
+    
+
+    return 0;
+}
